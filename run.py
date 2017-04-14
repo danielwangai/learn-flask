4fcd0765193b24b97550bb6fac823f633faa7667
Daniel Wangai <daniel.maina@andela.com>
2017-04-14 16:36:01 +0300

    [Chore] Add template and static files.

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World."
-
-if __name__ == '__main__':
+if __name__ == '__main_':
     app.run(debug=True)
