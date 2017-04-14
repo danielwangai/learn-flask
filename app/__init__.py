119a95be0d0d781cf2ae445f04558b1269d5c1b6
Daniel Wangai <daniel.maina@andela.com>
2017-04-14 19:06:41 +0300

    [Chore] Add login_manager object

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # local imports
 from config import app_config
@@ -8,11 +9,18 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    # initialize login manager object
+    login_manager.init_app(app)
+    login_manager.login_message("You must be logged in to access this page.")
+    login_manager.login_view('auth.login')
 
     @app.route("/")
     def index():
