bdc3268bdc7f2f0a6a0bd28512b5ba51cc174c97
Daniel Wangai <daniel.maina@andela.com>
2017-04-14 19:09:09 +0300

    [Chore] Add role model

--- app/models.py
+++ app/models.py
@@ -66,3 +66,20 @@ class Department(db.Model):
 
     def __repr__(self):
         return '<Department: {}>'.format(self.name)
+
+
+class Role(db.Model):
+    """
+    Define Role attributes
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
