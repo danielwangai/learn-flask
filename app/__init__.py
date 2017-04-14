de688b066edf849b55d877fa412795ad4bfd9757
Daniel Wangai <daniel.maina@andela.com>
2017-04-14 21:04:55 +0300

    [Chore] Render root page.

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,7 @@ def create_app(config_name):
 
     # register blueprints
     from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, '/admin')
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
