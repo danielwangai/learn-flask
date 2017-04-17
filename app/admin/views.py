f17d56d66dd13d848f2efacf114e5f8b24fde56d
Daniel Wangai <daniel.maina@andela.com>
2017-04-17 11:14:10 +0300

    [Chore] Edit admin views

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,7 +2,7 @@ from flask import abort, flash, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from . import admin
-from forms import DepartmentForm
+from .forms import DepartmentForm
 from .. import db
 from ..models import Department
 
@@ -43,7 +43,7 @@ def add_department():
 
     add_department = True
 
-    form = DepartmentForm
+    form = DepartmentForm()
     if form.validate_on_submit():
         department = Department(name=form.name.data,
                                 description=form.description.data)
@@ -62,7 +62,8 @@ def add_department():
                            add_department=add_department, form=form,
                            title="Add Department")
 
-@admin.route('/admin/departments/edit/<int:id>', methods=['GET', 'POST'])
+
+@admin.route('/departments/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_department(id):
     """
@@ -80,18 +81,19 @@ def edit_department(id):
         department.description = form.description.data
         db.session.commit()
         flash("You've successfully edited the department.")
+
         return redirect(url_for("admin.list_departments"))
 
     form.name.data = department.name
     form.description.data = department.description
 
-    return render_template("admin/departments/department", action="Edit",
+    return render_template("admin/departments/department.html", action="Edit",
                            add_department=add_department, form=form,
                            department=department, title="Edit Department")
 
 
 # delete a department
-@admin.route("/admin/departments/delete/<int:id>", methods=["GET", "POST"])
+@admin.route("/departments/delete/<int:id>", methods=["GET", "POST"])
 @login_required
 def delete_department(id):
     """
